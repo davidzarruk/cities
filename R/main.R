b21f6d788c0d1882c10034e8282d39a7675c5e5b
David Zarruk <davidzarruk@rappi.com>
2022-03-15 22:21:50 -0500

    update

--- R/main.R
+++ R/main.R
@@ -1,5 +1,5 @@
 
-setwd('/home/david/Documents/igc/')
+setwd('/home/david/Documents/cities/')
 rm(list = ls())
 
 source('R/utils.R')
@@ -57,22 +57,22 @@ lambda = 0.01
 tau = exp(lambda*time_mat)
 discount1 = 1-((exp(0.2*(distance_metro/150)))^(-1))*0.1
 discount_tr = t(discount1)
-tau_shock_metro = max((tau*discount1)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))))
+tau_shock_metro = pmax((tau*discount1)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))), 1)
 tau_hat_metro = tau_shock_metro/tau;
 
 discount2 = 1-((exp(0.25*(distance_monorail/150)))^(-1))*0.025;
 discount_tr = t(discount2)
-tau_shock_monorail = max((tau*discount2)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))))
+tau_shock_monorail = pmax((tau*discount2)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))), 1)
 tau_hat_monorail = tau_shock_monorail/tau
 
 discount3 = 1-((exp(0.25*(distance_etrain/150)))^(-1))*0.195
 discount_tr = t(discount3)
-tau_shock_etrain = max((tau*discount3)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))))
+tau_shock_etrain = pmax((tau*discount3)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))), 1)
 tau_hat_etrain = tau_shock_etrain/tau
 
 discount4 = discount1*discount2*discount3;
 discount_tr = t(discount4)
-tau_shock_all = max((tau*discount4)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))))
+tau_shock_all = pmax((tau*discount4)*kronecker(discount_tr, array(1, dim=c(dim(tau)[1]))), 1)
 tau_hat_all = tau_shock_all/tau
 
 
@@ -172,17 +172,152 @@ settings = list(tol=tol,
                 endo_Lr=endo_Lr)
 
 inversion_m_bl  = inversionModel_Eff(param,data,settings);
-data.A = inversion_m_bl[[1]];
-data.u = inversion_m_bl[[2]];
-data.u_init = inversion_m_bl[[2]];
-data.B = inversion_m_bl[[3]];
-settings.w_init = inversion_m_bl[[4]];
-# results_m_bl  = solveModel1_Eff(param,data,settings);
-# namelist = c("metro", "monorail", "etrain", "all")
-# tau_shock = cat(3,tau_shock_metro,tau_shock_monorail,tau_shock_etrain,tau_shock_all);
-# a = 1-cellfun('isempty',strfind(namelist,name));
-# index = find(a);
+data$A = inversion_m_bl$A;
+data$u = inversion_m_bl$u;
+data$u_init = inversion_m_bl$u;
+data$B = inversion_m_bl$B;
+settings$w_init = inversion_m_bl$w
 
+results_m_bl  = solveModel1_Eff(param,data,settings);
+
+name = 'monorail'
+namelist = c("metro", "monorail", "etrain", "all")
+
+tau_shock = array(0, dim=c(875, 875, 4))
+tau_shock[,,1] = tau_shock_metro
+tau_shock[,,2] = tau_shock_monorail
+tau_shock[,,3] = tau_shock_etrain
+tau_shock[,,4] = tau_shock_all
+
+a = namelist == name;
+index = which(a %in% TRUE)
+
+# Transit Intervention
+A = inversion_m_bl$A;
+u = inversion_m_bl$u;
+B = inversion_m_bl$B;
+w_init = inversion_m_bl$w;
+endo_Lr = 1;
+param    = list(alpha1=alpha1,
+                beta1=beta1,
+                beta_tilde=beta_tilde,
+                delta1=delta1,
+                theta1=theta1,
+                eta1=eta1,
+                kappa1=kappa1,
+                xi1=xi1,
+                sigma1=sigma1,
+                L_bar=L_bar,
+                H_bar=H_bar,
+                H_bar_rest=H_bar_rest,
+                N=N,
+                S=S)
+data     = list(tau=tau_shock[,, index],
+                A=A,
+                A_init=A_init,
+                u_init=u,
+                B=B,
+                B_init=B_init,
+                F=F,
+                A_tilde=A_tilde,
+                T=T,
+                lambda_i=lambda_i,
+                lambda_is_i=lambda_is_i,
+                L_j_data=workers_s)
+settings = list(tol=tol,
+                zeta=zeta,
+                w_init=w_init,
+                r_init=r_init,
+                q_init=q_init,
+                vartheta_init=vartheta_init,
+                endo_Lr=endo_Lr)
+
+results_m_shock  = solveModel1_Eff(param,data,settings);
+
+
+
+U_shock = results_m_shock$U_i/results_m_bl$U_i;
+lambda_shock = results_m_shock$lambda_i/results_m_bl$lambda_i;
+L_shock = results_m_shock$L_j/results_m_bl$L_j;
+U_shock_agg = results_m_shock$U/results_m_bl$U;
+Uhat_m = c(Uhat_m, U_shock);
+lambda_hat_m = c(lambda_hat_m, lambda_shock);
+L_hat_m = c(L_hat_m, L_shock);
+Uhat_m_agg = c(Uhat_m_agg, U_shock_agg);
+
+
+# Changing the restriction by x
+per_rest_set = c(0.5);
+for(i in 1:1){
+  per_rest = per_rest_set[i];
+  H_bar_rest = 18*(1+per_rest);
+  
+  param    = list(alpha1=alpha1,
+                  beta1=beta1,
+                  beta_tilde=beta_tilde,
+                  delta1=delta1,
+                  theta1=theta1,
+                  eta1=eta1,
+                  kappa1=kappa1,
+                  xi1=xi1,
+                  sigma1=sigma1,
+                  L_bar=L_bar,
+                  H_bar=H_bar,
+                  H_bar_rest=H_bar_rest,
+                  N=N,
+                  S=S)
+  data     = list(tau=tau_shock[,, index],
+                  A=A,
+                  A_init=A_init,
+                  u_init=u,
+                  B=B,
+                  B_init=B_init,
+                  F=F,
+                  A_tilde=A_tilde,
+                  T=T,
+                  lambda_i=lambda_i,
+                  lambda_is_i=lambda_is_i,
+                  L_j_data=workers_s)
+  settings = list(tol=tol,
+                  zeta=zeta,
+                  w_init=w_init,
+                  r_init=r_init,
+                  q_init=q_init,
+                  vartheta_init=vartheta_init,
+                  endo_Lr=endo_Lr)
+  
+  results_m_bl_shock  = solveModel1_GE_sim(param,data,settings);    
+  U_metro_agg_shock = results_m_bl_shock$U/results_m_bl$U;
+  
+  data     = list(tau=tau_shock[,, index],
+                  A=A,
+                  A_init=A_init,
+                  u_init=u,
+                  B=B,
+                  B_init=B_init,
+                  F=F,
+                  A_tilde=A_tilde,
+                  T=T,
+                  lambda_i=lambda_i,
+                  lambda_is_i=lambda_is_i,
+                  L_j_data=workers_s)
+  
+  results_m_metro  = solveModel1_GE_sim(param,data,settings);
+  
+  U_metro = results_m_metro$U_i/results_m_bl$U_i;
+  lambda_metro = results_m_metro$lambda_i/results_m_bl$lambda_i;
+  L_metro = results_m_metro$L_j/results_m_bl$L_j;
+  U_metro_agg = (results_m_metro$U/results_m_bl$U)-U_metro_agg_shock;
+  Uhat_m = c(Uhat_m, U_metro);
+  lambda_hat_m = c(lambda_hat_m, lambda_metro);
+  L_hat_m = c(L_hat_m, L_metro);
+  Uhat_m_agg = c(Uhat_m_agg, U_metro_agg);
+}
+
+fileU = ['results/Uhat_transit_' name '.csv'];
+fileL = ['results/L_hat_transit_' name '.csv'];
+fileR = ['results/lambda_hat_transit_' name '.csv'];
+fileUagg = ['results/Uhat_transit_agg_' name '.csv'];
 
 
 
--- R/utils.R
+++ R/utils.R
@@ -93,7 +93,7 @@ labor_productivity = function(param,data,settings){
     }
     print(paste(outerdiff, iter))
   }
-  return(list(w, w_tr, lambda_ijs_is))
+  return(list(w=w, w_tr=w_tr, lambda_ijs_is=lambda_ijs_is))
 }
 
 amenities = function(param,data,settings){
@@ -131,7 +131,66 @@ amenities = function(param,data,settings){
     }
     print(outerdiff)
   }
-  return(list(W_is, B))
+  return(list(W_is=W_is, B=B))
+}
+
+av_income = function(lambda_is_i, B, W_is, kappa1, y_bar, lambda_ijs_is, w, lambda_i){
+  lambda_is_i_p = aperm(kronecker(lambda_is_i, array(1, dim=c(1,1,1))), c(1, 3, 2));
+  y_bar = (sumDims(array_operator(B, W_is, '*')^(kappa1), 2))^(1/kappa1);
+  L_j_eff_p = sumDims(array_operator(y_bar, array_operator(lambda_i, array_operator(lambda_is_i_p, array_operator(lambda_ijs_is, L_bar, '*'), '*'), '*'), '*'),1);
+  L_j_eff_w = aperm(L_j_eff_p, c(2, 3, 1));
+  L_j_eff = array_operator(L_j_eff_w, w, '/');
+  return(list(lambda_is_i_p=lambda_is_i_p, L_j_eff_p=L_j_eff_p, L_j_eff_w=L_j_eff_w, L_j_eff=L_j_eff, y_bar=y_bar))
+}
+
+floorspace_supply = function(delta1, H_bar_rest, H_barr, w, L_j_eff, y_bar, lambda_i, L_bar, param){
+  gamma1=array_operator(delta1, (1-delta1), '/');
+  H_r = pmin(H_bar_rest,H_bar);
+  H_f = pmin(H_bar_rest,H_bar);
+  beta_tilde_sector = aperm(array_operator((1-param$beta1), param$beta1, '/'), c(1, 3, 2));
+  q = (array_operator(sumDims(array_operator(beta_tilde_sector, array_operator(w, L_j_eff, '*'), '*'),2), H_f, '/'));
+  r = ((1-alpha1)/alpha1)*(array_operator(array_operator(y_bar, lambda_i*L_bar, '*')+array_operator(q, H_f, '*'), H_r, '/'));        
+  return(list(beta_tilde_sector=beta_tilde_sector, q=q, r=r, H_r=H_r, H_f=H_f))
+}
+
+number_firms = function(beta1, beta_tilde_sector, w, L_j_eff, sigma1, q){
+  beta_tilde = aperm(array_operator(array_operator(beta1, (-beta1), '^'), (array_operator((1-beta1), (-(1-beta1)), '^')), '*'), c(1, 3, 2));
+  H_f_sector = array_operator((array_operator(kronecker(beta_tilde_sector, array(1, dim=c(875,1,1))), sumDims(array_operator(w, L_j_eff, '*'), 2), '*')), q, '/');
+  sigma_cons = aperm(sigma1, c(1, 3, 2)); 
+  beta_factor = aperm(beta1, c(1, 3, 2));
+  M = array_operator(array_operator(array_operator(beta_tilde, array_operator(L_j_eff, (beta_factor), '^'), '*'), array_operator(H_f_sector,(1-beta_factor), '^'), '*'), array_operator(sigma_cons, F, '*'), '/');        
+  return(list(beta_factor=beta_factor, M=M, sigma_cons=sigma_cons, beta_tilde=beta_tilde))
+}
+
+price_location = function(sigma1, beta_factor, w, q, A){
+  markup = aperm(array_operator(sigma1, (sigma1-1), '/'), c(1, 3, 2));
+  inv_sigma = aperm((1/(1-sigma1)), c(1, 3, 2));
+  p_j = array_operator(array_operator(array_operator(markup, array_operator(w, beta_factor, '^'), '*'), array_operator(q, kronecker(1-beta_factor, array(1, dim=c(875,1,1))), '^'), '*'), A, '/');        
+  return(list(p_j=p_j, inv_sigma=inv_sigma, markup=markup))
+}
+
+agg_price_index = function(sigma1, M, p_j, sigma1_tr, inv_sigma){
+  sigma1_tr = aperm(sigma1, c(1, 3, 2));
+  P_s = array_operator((sumDims(array_operator(M, array_operator(p_j, (1-sigma1_tr), '^'), '*'),1)), (inv_sigma), '^');
+  pi_js = array_operator(array_operator(M, array_operator(p_j, (1-sigma1_tr), '^'), '*'), (array_operator(P_s, (1-sigma1_tr), '^')), '/');        
+  P = array_operator(sumDims(array_operator(P_s, (1-xi1), '^'),2), (1/(1-xi1)), '^');
+  pi_s = array_operator(array_operator(P_s, (1-xi1), '^'), (array_operator(P, (1-xi1), '^')), '/');
+  return(list(pi_js=pi_js, pi_s=pi_s, P=P, P_s=P_s, sigma1_tr=sigma1_tr))
+}
+
+total_sales = function(y_bar, lambda_i, L_bar, r, H_r, q, H_f, alpha1, pi_js, pi_s){
+  X = sumDims(array_operator(y_bar, array_operator(lambda_i, kronecker(L_bar, array(1, dim=c(875))), '*'), '*') + array_operator(r, H_r, '*') +array_operator(q, H_f, '*'), 1);
+  Y_js = array_operator(alpha1, array_operator(pi_js, array_operator(pi_s, X, '*'), '*'), '*');
+  return(list(X=X, Y_js=Y_js))
+}
+
+new_labor_demand = function(sigma_cons, w, L_j_eff, beta_factor, Y_js, A, inv_sigma){
+  inv_sigma = array_operator(sigma_cons-1, -1, '^');
+  LS = array_operator(w, L_j_eff, '*');
+  LD = array_operator(beta_factor, Y_js, '*');
+  LD_A = array_operator(LD, array_operator(A, (sigma_cons-1), '^'), '/');
+  A_prime = array_operator(array_operator(LS, LD_A, '/'), inv_sigma, '^');
+  return(list(inv_sigma=inv_sigma, LS=LS, LD=LD, LD_A=LD_A, A_prime=A_prime))
 }
 
 eq_quantities = function(param,data,settings){
@@ -147,51 +206,48 @@ eq_quantities = function(param,data,settings){
   lambda_ijs_is = param$lambda_ijs_is
   
   # 2) Average income in each location
-  lambda_is_i_p = aperm(kronecker(lambda_is_i, array(1, dim=c(1,1,1))), c(1, 3, 2));
-  y_bar = (sumDims(array_operator(B, W_is, '*')^(kappa1), 2))^(1/kappa1);
-  L_j_eff_p = sumDims(array_operator(y_bar, array_operator(lambda_i, array_operator(lambda_is_i_p, array_operator(lambda_ijs_is, L_bar, '*'), '*'), '*'), '*'),1);
-  L_j_eff_w = aperm(L_j_eff_p, c(2, 3, 1));
-  L_j_eff = array_operator(L_j_eff_w, w, '/');
+  av_inc = av_income(lambda_is_i, B, W_is, kappa1, y_bar, lambda_ijs_is, w, lambda_i)
+  L_j_eff = av_inc$L_j_eff
+  y_bar = av_inc$y_bar
   
   # 3) Floorspace supply
-  #delta_tilde = (delta1.^(-delta1)).*((1-delta1).^(-(1-delta1)));
-  gamma1=array_operator(delta1, (1-delta1), '/');
-  H_r = pmin(H_bar_rest,H_bar);
-  H_f = pmin(H_bar_rest,H_bar);
-  beta_tilde_sector = aperm(array_operator((1-param$beta1), param$beta1, '/'), c(1, 3, 2));
-  q = (array_operator(sumDims(array_operator(beta_tilde_sector, array_operator(w, L_j_eff, '*'), '*'),2), H_f, '/'));
-  r = ((1-alpha1)/alpha1)*(array_operator(array_operator(y_bar, lambda_i*L_bar, '*')+array_operator(q, H_f, '*'), H_r, '/'));        
+  floorspace_sup = floorspace_supply(delta1, H_bar_rest, H_barr, w, L_j_eff, y_bar, lambda_i, L_bar, param)
+  beta_tilde_sector = floorspace_sup$beta_tilde_sector
+  q = floorspace_sup$q
+  r = floorspace_sup$r
+  H_r = floorspace_sup$H_r
+  H_f = floorspace_sup$H_f
   
   # 5) Number of firms by location
-  beta_tilde = aperm(array_operator(array_operator(beta1, (-beta1), '^'), (array_operator((1-beta1), (-(1-beta1)), '^')), '*'), c(1, 3, 2));
-  H_f_sector = array_operator((array_operator(kronecker(beta_tilde_sector, array(1, dim=c(875,1,1))), sumDims(array_operator(w, L_j_eff, '*'), 2), '*')), q, '/');
-  sigma_cons = aperm(sigma1, c(1, 3, 2)); 
-  beta_factor = aperm(beta1, c(1, 3, 2));
-  M = array_operator(array_operator(array_operator(beta_tilde, array_operator(L_j_eff, (beta_factor), '^'), '*'), array_operator(H_f_sector,(1-beta_factor), '^'), '*'), array_operator(sigma_cons, F, '*'), '/');        
+  number_fir = number_firms(beta1, beta_tilde_sector, w, L_j_eff, sigma1, q)
+  beta_factor = number_fir$beta_factor
+  M = number_fir$M
+  sigma_cons = number_fir$sigma_cons
   
   while(outerdiff>1e-06 & iter < 10){
     # 6) Price in each location
-    markup = aperm(array_operator(sigma1, (sigma1-1), '/'), c(1, 3, 2));
-    inv_sigma = aperm((1/(1-sigma1)), c(1, 3, 2));
-    p_j = array_operator(array_operator(array_operator(markup, array_operator(w, beta_factor, '^'), '*'), array_operator(q, kronecker(1-beta_factor, array(1, dim=c(875,1,1))), '^'), '*'), A, '/');        
+    price_loc = price_location(sigma1, beta_factor, w, q, A)
+    p_j = price_loc$p_j
+    inv_sigma = price_loc$inv_sigma
     
     # 7) Aggregate price index and expenditure share in the variety of each location
-    sigma1_tr = aperm(sigma1, c(1, 3, 2));
-    P_s = array_operator((sumDims(array_operator(M, array_operator(p_j, (1-sigma1_tr), '^'), '*'),1)), (inv_sigma), '^');
-    pi_js = array_operator(array_operator(M, array_operator(p_j, (1-sigma1_tr), '^'), '*'), (array_operator(P_s, (1-sigma1_tr), '^')), '/');        
-    P = array_operator(sumDims(array_operator(P_s, (1-xi1), '^'),2), (1/(1-xi1)), '^');
-    pi_s = array_operator(array_operator(P_s, (1-xi1), '^'), (array_operator(P, (1-xi1), '^')), '/');
+    agg_price_ind = agg_price_index(sigma1, M, p_j, sigma1_tr, inv_sigma)
+    pi_js = agg_price_ind$pi_js
+    pi_s = agg_price_ind$pi_s
+    P = agg_price_ind$P
     
     # 8) Total sales in each location
-    X = sumDims(array_operator(y_bar, array_operator(lambda_i, kronecker(L_bar, array(1, dim=c(875))), '*'), '*') + array_operator(r, H_r, '*') +array_operator(q, H_f, '*'), 1);
-    Y_js = array_operator(alpha1, array_operator(pi_js, array_operator(pi_s, X, '*'), '*'), '*');
+    total_sal = total_sales(y_bar, lambda_i, L_bar, r, H_r, q, H_f, alpha1, pi_js, pi_s)
+    X = total_sal$X
+    Y_js = total_sal$Y_js
     
     # 9) New labor demand
-    inv_sigma = array_operator(sigma_cons-1, -1, '^');
-    LS = array_operator(w, L_j_eff, '*');
-    LD = array_operator(beta_factor, Y_js, '*');
-    LD_A = array_operator(LD, array_operator(A, (sigma_cons-1), '^'), '/');
-    A_prime = array_operator(array_operator(LS, LD_A, '/'), inv_sigma, '^');
+    new_labor_dem = new_labor_demand(sigma_cons, w, L_j_eff, beta_factor, Y_js, A, inv_sigma)
+    inv_sigma = new_labor_dem$inv_sigma
+    LS = new_labor_dem$LS
+    LD = new_labor_dem$LD
+    LD_A = new_labor_dem$LD_A
+    A_prime = new_labor_dem$A_prime
     
     # 10) Update
     z_A = array_operator(A, A_prime, '-');
@@ -253,37 +309,306 @@ last_eq_quantities = function(param,data,settings){
     outerdiff = diff_u  
     print(outerdiff)
   }
+  return(list(u=u))
 }
 
 inversionModel_Eff = function(param,data,settings){
 
   # Labor productivity
   LP = labor_productivity(param,data,settings)
-  param$w = LP[[1]]
-  param$w_tr = LP[[2]]
+  param$w = LP$w
+  param$w_tr = LP$w_tr
   
   param$reptau = kronecker(data$tau, array(1, dim=c(1, 1, param$S)))
   
   # Amenities
   AM = amenities(param,data,settings)
   param$zeta_init = settings$zeta
-  param$W_is = AM[[1]]
-  param$B = AM[[2]]
-  param$lambda_ijs_is = LP[[3]]
+  param$W_is = AM$W_is
+  param$B = AM$B
+  param$lambda_ijs_is = LP$lambda_ijs_is
   
   # Other equilibrium quantities
+  print("eq quantities starting")
   EQ = eq_quantities(param,data,settings)
   param$U_i = EQ[[1]]
   A = EQ[[2]]
   
-  
-  last_eq_quantities(param,data,settings)
+  LEQ = last_eq_quantities(param,data,settings)
+  u = LEQ$u
   
   # Save and export
   U = (sumDims(array_operator(u, array_operator(param$U_i, eta1, '^'), '*'),1))^(1/eta1)
-  # results = v2struct(u,A,w,B,r,q,pi_js,pi_s,lambda_ijs_is,lambda_is_i,H_r,H_f,U,U_i,lambda_i,L_j,LD,LS,y_bar);
-  # return(results)
-  
-  return(list(A, U, param$B, param$w))
+
+  return(list(A=A, u=u, B=param$B, w=param$w))
 }
 
+
+solveModel1_Eff = function(param,data,settings){
+  # Solve baseline model
+
+  # Settings
+  outerdiff = Inf;
+  w = settings$w_init;
+  u = data$u_init;
+  iter = 0;
+  zeta_init = settings$zeta;
+
+  while(outerdiff>tol & iter < 10){
+    # 1) Labor supply
+    # Indirect utility
+    w_tr = aperm(array(w, dim=c(875,3,1)), c(3,1,2))
+    reptau = kronecker(data$tau, array(1, dim=c(1, 1, param$S)))
+    
+    w_tr_reptau = array_operator(w_tr^theta1, reptau^(-theta1), '*')
+    lambda_ijs_is = array_operator(w_tr_reptau, sumDims(w_tr_reptau, 2), '/')
+
+    W_is_aux_1 = sumDims(w_tr_reptau,2);
+    W_is_aux = W_is_aux_1^(1/theta1);
+    W_is = aperm(W_is_aux, c(1,3,2));
+    
+    B_W_is = array_operator(data$B, W_is, '*')^(kappa1)
+    lambda_is_i = array_operator(B_W_is, sumDims(B_W_is,2), '/');
+    
+    lambda_is_i_p = aperm(kronecker(data$lambda_is_i, array(1, dim=c(1,1,1))), c(1, 3, 2));
+    
+    l_prod = array_operator(data$lambda_i, array_operator(lambda_is_i_p, lambda_ijs_is, '*'), '*')
+    L_j_p = sumDims(l_prod, 1)
+    L_j = aperm(L_j_p, c(2, 3, 1));
+
+    # 2) Average income in each location
+    av_inc = av_income(lambda_is_i, data$B, W_is, kappa1, y_bar, lambda_ijs_is, w, lambda_i);
+    lambda_is_i_p=av_inc$lambda_is_i_p
+    L_j_eff_p=av_inc$L_j_eff_p
+    L_j_eff_w=av_inc$L_j_eff_w
+    L_j_eff=av_inc$L_j_eff
+    y_bar=av_inc$y_bar
+
+    # 3) Floorspace supply
+    floorspace = floorspace_supply(delta1, H_bar_rest, H_barr, w, L_j_eff, y_bar, lambda_i, L_bar, param)
+    H_r = floorspace$H_r
+    H_f = floorspace$H_f
+    beta_tilde_sector = floorspace$beta_tilde_sector
+    q = floorspace$q
+    r = floorspace$r
+
+    # 5) Number of firms by location
+    num_firms = number_firms(beta1, beta_tilde_sector, w, L_j_eff, sigma1, q)
+    beta_factor=num_firms$beta_factor
+    M=num_firms$M
+    sigma_cons=num_firms$sigma_cons
+    
+    H_f_sector = array_operator(array_operator(kronecker(beta_tilde_sector, array(1, dim=c(875,1,1))), sumDims(array_operator(w, L_j_eff, '*'),2), '*'), q, '/');
+
+    Q_js = array_operator(array_operator(L_j_eff, beta_factor, '^'), array_operator(H_f_sector, 1-beta_factor, '^'), '*');
+    elast_demand = aperm(array_operator(sigma1, sigma1-1, '/'), c(1,3,2));
+    inv_elast_demand = aperm(array_operator(sigma1-1, sigma1, '/'), c(1,3,2));
+    
+    Q_s = array_operator(sumDims(array_operator(Q_js, elast_demand, '^'), 1), inv_elast_demand, '^');
+    Q = array_operator(sumDims(array_operator(Q_s, (xi1-1)/xi1, '^'),2), xi1/(xi1-1), '^');
+    
+    # 6) Price in each location
+    price_loc = price_location(sigma1, beta_factor, w, q, data$A)
+    p_j = price_loc$p_j
+    inv_sigma = price_loc$inv_sigma
+
+    # 7) Aggregate price index and expenditure share in the variety of each location
+    agg_price_ind = agg_price_index(sigma1, M, p_j, sigma1_tr, inv_sigma)
+    pi_js = agg_price_ind$pi_js
+    pi_s = agg_price_ind$pi_s
+    P = agg_price_ind$P
+    P_s = agg_price_ind$P_s
+    
+    # 8) Total sales in each location
+    total_sal = total_sales(y_bar, lambda_i, L_bar, r, H_r, q, H_f, alpha1, pi_js, pi_s)
+    X = total_sal$X
+    Y_js = total_sal$Y_js
+
+    # 9) New labor demand and wages
+    new_labor_dem = new_labor_demand(sigma_cons, w, L_j_eff, beta_factor, Y_js, A, inv_sigma)
+    inv_sigma = new_labor_dem$inv_sigma
+    LS = new_labor_dem$LS
+    LD = new_labor_dem$LD
+    w_prime = array_operator(LD, L_j_eff, '/');
+    w_prime = array_operator(w_prime, w_prime[1,1,1], '/');
+
+    # 10) Update
+    w = zeta*w_prime + (1-zeta)*w;
+    w = array_operator(w, w[1,1,1], '/');
+
+    w_diff = max(abs(w - w_prime));
+
+    num_U_i = (sumDims(array_operator(W_is, kappa1, '^'),2))^(1/kappa1);
+    den_U_i = array_operator((r^(1-alpha1)), (P^(alpha1)), '*');
+    U_i = array_operator(num_U_i, den_U_i, '/');
+    U = array_operator(sumDims(array_operator(u, U_i^eta1, '*'),1), 1/eta1, '^');
+
+    if(settings$endo_Lr==1){
+      lambda_i_upd = array_operator(array_operator(u, U_i^eta1, '*'), sumDims(array_operator(u, U_i^eta1, '*'),1), '/');
+      lambda_diff = max(abs(lambda_i-lambda_i_upd));
+      lambda_i = 0.05*lambda_i_upd+(1-0.05)*lambda_i;
+    }
+    
+    outerdiff = max(max(w_diff), max(lambda_diff))
+    iter = iter + 1;
+
+    if(outerdiff<500){
+      zeta = zeta_init*10;
+    }
+    
+    if(outerdiff<300){
+      zeta = zeta_init*50;
+    }
+    
+    if(outerdiff<100){
+      zeta = zeta_init*100;
+    }
+    
+    if(outerdiff<20){
+      zeta = zeta_init*500;
+    }
+    
+    if(outerdiff<10){
+      zeta = zeta_init*500;
+    }
+    
+    if(outerdiff<1){
+      zeta = zeta_init*500;
+    }
+    print(outerdiff)
+  }
+  
+  # Save and export
+  M_sum = sumDims(M,1);
+  return(list(w=w, W_is=W_is, B=B, r=r, q=q, pi_js=pi_js, pi_s=pi_s, lambda_ijs_is=lambda_ijs_is,
+         y_bar=y_bar, lambda_is_i=lambda_is_i, H_r=H_r, H_f=H_f, U=U, U_i=U_i,
+         lambda_i=lambda_i, L_j=L_j, H_bar=H_bar, H_bar_rest=H_bar_rest, Q_js=Q_js,
+         Q_s=Q_s, Q=Q, L_j_eff=L_j_eff, M_sum=M_sum))
+}
+
+
+
+
+solveModel1_GE_sim=function(param,data,settings){
+  # Settings
+  outerdiff = Inf;    
+  w = w_init;
+  u = u_init;
+  iter = 0;
+  zeta_init = zeta;
+  
+  while(outerdiff>tol & iter < 10){
+
+    w_tr = aperm(array(w, dim=c(875,3,1)), c(3,1,2))
+    reptau = kronecker(data$tau, array(1, dim=c(1, 1, param$S)))
+    
+    w_tr_reptau = array_operator(w_tr^theta1, reptau^(-theta1), '*')
+    lambda_ijs_is = array_operator(w_tr_reptau, sumDims(w_tr_reptau, 2), '/')
+    
+    W_is_aux_1 = sumDims(w_tr_reptau,2);
+    W_is_aux = W_is_aux_1^(1/theta1);
+    W_is = aperm(W_is_aux, c(1,3,2));
+
+    B_W_is = array_operator(data$B, W_is, '*')^(kappa1)
+    lambda_is_i = array_operator(B_W_is, sumDims(B_W_is,2), '/');
+    
+    lambda_is_i_p = aperm(kronecker(lambda_is_i, array(1, dim=c(1,1,1))), c(1, 3, 2));
+    
+    l_prod = array_operator(data$lambda_i, array_operator(lambda_is_i_p, lambda_ijs_is, '*'), '*')
+    L_j_p = sumDims(l_prod, 1)
+    L_j = aperm(L_j_p, c(2, 3, 1));
+    
+
+    # 2) Average income in each location
+    av_inc = av_income(lambda_is_i, data$B, W_is, kappa1, y_bar, lambda_ijs_is, w, lambda_i);
+    lambda_is_i_p=av_inc$lambda_is_i_p
+    y_bar=av_inc$y_bar
+    L_j_eff=av_inc$L_j_eff
+    
+    # 3) Floorspace supply
+    floorspace = floorspace_supply(delta1, H_bar_rest, H_barr, w, L_j_eff, y_bar, lambda_i, L_bar, param)
+    H_r = floorspace$H_r
+    H_f = floorspace$H_f
+    beta_tilde_sector = floorspace$beta_tilde_sector
+    q = floorspace$q
+    r = floorspace$r    
+
+    # 5) Number of firms by location
+    num_firms = number_firms(beta1, beta_tilde_sector, w, L_j_eff, sigma1, q)
+    beta_tilde=num_firms$beta_tilde
+    beta_factor=num_firms$beta_factor
+    M=num_firms$M
+    sigma_cons=num_firms$sigma_cons
+    
+    H_f_sector = array_operator(array_operator(kronecker(beta_tilde_sector, array(1, dim=c(875,1,1))), sumDims(array_operator(w, L_j_eff, '*'),2), '*'), q, '/');
+
+    # 6) Price in each location
+    price_loc = price_location(sigma1, beta_factor, w, q, data$A)
+    p_j = price_loc$p_j
+    inv_sigma = price_loc$inv_sigma    
+    markup=price_loc$markup
+    
+    # 7) Aggregate price index and expenditure share in the variety of each location
+    agg_price_ind = agg_price_index(sigma1, M, p_j, sigma1_tr, inv_sigma)
+    pi_js = agg_price_ind$pi_js
+    pi_s = agg_price_ind$pi_s
+    P = agg_price_ind$P
+    P_s = agg_price_ind$P_s
+    sigma1_tr = agg_price_ind$sigma1_tr
+    
+    # 8) Total sales in each location
+    total_sal = total_sales(y_bar, lambda_i, L_bar, r, H_r, q, H_f, alpha1, pi_js, pi_s)
+    X = total_sal$X
+    Y_js = total_sal$Y_js
+    
+    # 9) New labor demand and wages
+    new_labor_dem = new_labor_demand(sigma_cons, w, L_j_eff, beta_factor, Y_js, A, inv_sigma)
+    LS = new_labor_dem$LS
+    LD = new_labor_dem$LD
+    w_prime = array_operator(LD, L_j_eff, '/');
+    w_prime = array_operator(w_prime, w_prime[1,1,1], '/');
+    
+    # 10) Update
+    w = zeta*w_prime + (1-zeta)*w;
+    w = array_operator(w, w[1,1,1], '/');
+    
+    w_diff = max(abs(w - w_prime));
+    
+    num_U_i = (sumDims(array_operator(W_is, kappa1, '^'),2))^(1/kappa1);
+    den_U_i = array_operator((r^(1-alpha1)), (P^(alpha1)), '*');
+    U_i = array_operator(num_U_i, den_U_i, '/');
+    U = array_operator(sumDims(array_operator(u, U_i^eta1, '*'),1), 1/eta1, '^');
+    
+    if(settings$endo_Lr==1){
+      lambda_i = array_operator(array_operator(u, U_i^eta1, '*'), sumDims(array_operator(u, U_i^eta1, '*'),1), '/')
+    }
+
+    outerdiff = max(w_diff)
+    iter = iter + 1;
+    
+    if(outerdiff<500){
+      zeta = zeta_init*10;
+    }
+    
+    if(outerdiff<300){
+      zeta = zeta_init*50;
+    }
+    
+    if(outerdiff<100){
+      zeta = zeta_init*100;
+    }
+    
+    if(outerdiff<20){
+      zeta = zeta_init*500;
+    }
+    
+    if(outerdiff<10){
+      zeta = zeta_init*500;
+    }
+    
+    if(outerdiff<1){
+      zeta = zeta_init*500;
+    }
+    print(outerdiff)
+  }
+}
